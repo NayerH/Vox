0452cc8afa20295a270f87931516c507ddf5377d
= <nayerhany@gmail.com>
2023-04-22 16:09:15 +0200

    Initial Commit

---
+++ src/main/java/com/vox/post/repository/Post.java
@@ -0,0 +1,15 @@
+package com.vox.post.repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}
