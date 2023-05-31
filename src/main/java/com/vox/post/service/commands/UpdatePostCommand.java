de31be3ad690e4cafadab7b6307af6328a42de4a
OmarAreef <omar.mousaaref@gmail.com>
2023-05-31 03:55:46 +0300

    added mq connections

--- src/main/java/com/vox/post/service/commands/UpdatePostCommand.java
+++ src/main/java/com/vox/post/service/commands/UpdatePostCommand.java
@@ -1,21 +1,37 @@
 package com.vox.post.service.commands;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vox.post.exception.ApiRequestException;
 import com.vox.post.model.Category;
 import com.vox.post.model.Post;
 import com.vox.post.repository.posts.PostRepository;
 import com.vox.post.service.interfaces.UpdateCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 
 @Component
 public class UpdatePostCommand implements UpdateCommand {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UpdatePostCommand.class);
+
+    @Value("${rabbitmq.searchQueueRouting.key}")
+    private String searchPostsQueueRoutingKey;
+
+    @Value("${rabbitmq.searchExchange.name}")
+    private String searchExchange;
+    private final RabbitTemplate rabbitTemplate;
     private final PostRepository postRepository;
     @Autowired
-    public UpdatePostCommand(PostRepository postRepository) {
+    public UpdatePostCommand(PostRepository postRepository ,  RabbitTemplate rabbitTemplate) {
         this.postRepository = postRepository;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     @Override
@@ -39,6 +55,21 @@ public class UpdatePostCommand implements UpdateCommand {
         }
         if(category != null){
             post.setCategory(category);
+        }
+        LinkedHashMap<String, Object> rabbitMessage = new LinkedHashMap<>();
+        rabbitMessage.put("post", post);
+        rabbitMessage.put("id", post.getId().toString());
+        rabbitMessage.put("command", "update");
+        LOGGER.info(String.format("Sent JSON message -> %s", rabbitMessage));
+        ObjectMapper mapper = new ObjectMapper();
+        // Try block to check for exceptions
+        try {
+            String postJsonString
+                    = mapper.writeValueAsString(rabbitMessage);
+            rabbitTemplate.convertAndSend(searchExchange, searchPostsQueueRoutingKey, postJsonString );
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
         this.postRepository.save(post);
         return post;
